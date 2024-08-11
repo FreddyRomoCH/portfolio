6f551ce6a22ea26484704b9045641962eecc4a14
Freddy Romo <freddyromochavez@gmail.com>
2024-08-11 19:57:15 +0300

    Closing Menu mobile when clicking logo

--- src/components/Aside.jsx
+++ src/components/Aside.jsx
@@ -9,7 +9,7 @@ export function Aside({ isMobileMenuOpen, onClickOpenMenu }) {
         isMobileMenuOpen ? "translate-x-0" : "-translate-x-full"
       } fixed top-0 flex z-20 bg-secondary900 md:flex flex-col justify-between h-full transition-all ease-linear duration-500 md:translate-x-0 px-10 md:px-0 w-dvw md:w-[23%]`}
     >
-      <Logo />
+      <Logo onClickOpenMenu={onClickOpenMenu} />
       <Menu onClickOpenMenu={onClickOpenMenu} />
       <Social />
     </aside>
--- src/components/Aside/Logo.jsx
+++ src/components/Aside/Logo.jsx
@@ -1,10 +1,10 @@
 import { Link } from "react-router-dom";
 
-export function Logo() {
+export function Logo({ onClickOpenMenu }) {
   return (
     <header className="flex justify-center bg-primary400 w-full h-12">
       <picture>
-        <Link to="/">
+        <Link onClick={onClickOpenMenu} to="/">
           <img src="/images/logo.png" alt="Freddy Romo Logo" className="w-20" />
         </Link>
       </picture>
