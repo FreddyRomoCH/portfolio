5fbc52061672b779eee78a71858fea666ce78250
Freddy Romo <freddyromochavez@gmail.com>
2024-08-11 21:04:16 +0300

    Adding style current section button

--- src/components/Aside/Menu.jsx
+++ src/components/Aside/Menu.jsx
@@ -1,7 +1,11 @@
+import { useLocation } from "react-router-dom";
 import { MenuButton } from "./MenuButton";
 
 export function Menu({ onClickOpenMenu }) {
   const CV = "files/Freddy-Romo-Resume.pdf";
+
+  const url = useLocation();
+  const currentSection = url.pathname.slice(1);
 
   return (
     <section className="menu">
@@ -9,18 +13,16 @@ export function Menu({ onClickOpenMenu }) {
         <MenuButton
           title="About"
           href="/about"
+          isActive={currentSection === "about"}
           onClickOpenMenu={onClickOpenMenu}
         />
         <MenuButton
           title="Projects"
           href="/projects"
+          isActive={currentSection === "projects"}
           onClickOpenMenu={onClickOpenMenu}
         />
-        <MenuButton
-          title="Contact"
-          href="#contact"
-          onClickOpenMenu={onClickOpenMenu}
-        />
+        <MenuButton title="Contact" onClickOpenMenu={onClickOpenMenu} />
         <MenuButton title="My CV" href={CV} onClickOpenMenu={onClickOpenMenu} />
       </ul>
     </section>
--- src/components/Aside/MenuButton.jsx
+++ src/components/Aside/MenuButton.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 import { SendEmail } from "../home/SendEmail";
 import { useState } from "react";
 
-export function MenuButton({ title, href, onClickOpenMenu }) {
+export function MenuButton({ title, href, onClickOpenMenu, isActive }) {
   const [iscontactOpen, setIsContactOpen] = useState(false);
 
   const handleClickContact = () => {
@@ -10,7 +10,13 @@ export function MenuButton({ title, href, onClickOpenMenu }) {
   };
 
   return (
-    <li className="relative border-[1px] border-primary400 hover:border-primary600 text-primary400 hover:text-primary600 font-normal text-xl 2xl:text-2xl w-full text-center">
+    <li
+      className={`relative border-[1px] border-primary400 hover:border-primary600 hover:text-primary600 font-normal text-xl 2xl:text-2xl w-full text-center ${
+        isActive
+          ? "bg-primary500 text-neutral50 hover:bg-primary600 hover:text-neutral50"
+          : "text-primary400 "
+      }`}
+    >
       {title === "My CV" ? (
         <a
           href={href}
