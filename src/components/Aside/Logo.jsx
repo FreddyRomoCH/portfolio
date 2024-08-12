b8a4e90a33212f5e65e76ee27a86645e9889dc0d
Freddy Romo <freddyromochavez@gmail.com>
2024-08-12 13:34:42 +0300

    Adding Logo and FavIcon

--- src/components/Aside/Logo.jsx
+++ src/components/Aside/Logo.jsx
@@ -2,10 +2,14 @@ import { Link } from "react-router-dom";
 
 export function Logo({ onClickOpenMenu }) {
   return (
-    <header className="flex justify-center bg-primary400 w-full h-12">
+    <header className="flex bg-primary500 justify-center w-full  p-1">
       <picture>
         <Link onClick={onClickOpenMenu} to="/">
-          <img src="/images/logo.png" alt="Freddy Romo Logo" className="w-20" />
+          <img
+            src="/freddy-logo-2.svg"
+            alt="Freddy Romo Dev - Logo"
+            className="w-20"
+          />
         </Link>
       </picture>
     </header>
