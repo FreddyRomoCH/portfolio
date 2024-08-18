1f132b3056c08da09266d13d5c3256f418bc68b2
Freddy Romo <freddyromochavez@gmail.com>
2024-08-18 20:07:21 +0300

    Changing projects url in section projects

--- src/services/projects.js
+++ src/services/projects.js
@@ -5,7 +5,7 @@ export const projects = [
         shortDescription: "Fanpage of the band Metallica.",
         description: "Fanpage of the band Metallica. It uses Astro to generate static pages and React to create dinamics components. It fetches data from the Spotify, SetlistFM and LastFM APIs to show the albums and songs and latest live concerts of the band.",
         image: "/images/projects/metallica.png",
-        url: "https://metallica.onrender.com/",
+        url: "https://metallica.freddyromo.dev/",
         technologies: ["Astro", "React", "TailwindCSS", "API Fetch"],
         gitHub: "https://github.com/FreddyRomoCH/metallica"
     },
@@ -15,7 +15,7 @@ export const projects = [
         shortDescription: "Web Site to store recipes (Still in progress).",
         description: "Web Site built with React to store recipes. It uses React Router to navigate between pages and TailwindCSS for styling. In the future it will have backend to add recipes manually and a login system where you'll be able to manage your own recipes.",
         image: "/images/projects/my-recipes.png",
-        url: "https://my-own-recipes.onrender.com/",
+        url: "https://recipes.freddyromo.dev/",
         technologies: ["React", "TailwindCSS", "React Router", "UseContext", "API Fetch"],
         gitHub: "https://github.com/FreddyRomoCH/my-own-recipes"
     },
