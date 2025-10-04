b5c47edc995a5ed1ecf432232017dd3b86ce23c6
Freddy Romo <freddyromochavez@gmail.com>
2025-10-04 15:11:08 +0300

    Adjust 404 erro on mobile

--- src/components/404/NotFound.jsx
+++ src/components/404/NotFound.jsx
@@ -1,10 +1,10 @@
 export function NotFound() {
   return (
-    <section className="md:col-span-9 md:text-center px-4 lg:px-10 my-8 flex flex-col items-center justify-center gap-4">
+    <section className="md:col-span-9 md:text-center px-4 lg:px-10 flex flex-col items-center justify-center gap-4 h-dvh">
       <h1 className="text-5xl md:text-7xl font-bold text-primary400">404</h1>
       <p className="text-2xl md:text-3xl text-primary400">Page not found.</p>
 
-      <p className="mb-4 text-neutral-50 font-sans text-md font-light max-w-2xl">
+      <p className="mb-4 text-neutral-50 font-sans text-md font-light max-w-2xl text-center">
         The link may be broken or the page was moved. Go back home or browse my
         projects.
       </p>
